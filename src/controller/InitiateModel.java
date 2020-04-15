2effc5f39ef0fa6f977d6522e9abe834392bf6b6
Snir Yehuda <SnirYehud@Gmail.com>
2020-04-15 21:44:19 +0300

    added initiate servlet

---
+++ src/controller/InitiateModel.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InitiateModel
+ */
+@WebServlet("/InitiateModel")
+@SuppressWarnings("serial")
+public class InitiateModel extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	public void init() throws ServletException
+	{
+		System.out.println("---------- Here shoud come the code that create model instance----------");
+//		model.Model myModel=new model.Model();
+//		getServletContext().setAttribute("model", myModel);
+	}
+}
