3840d8adaab08d926685ecf354de5050a7f83097
Idan Shpirer <Shpirer.idan@Gmail.com>
2020-04-15 22:17:25 +0300

    after pesach

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,6 +1,7 @@
 package model;
 import java.sql.*;
 
+
 public class Model {
 	Connection myCon;
 	String[] users_columns = {"last_name", "first_name", "user_name", "password", "email", "city", "address", "phone", "gender", "privileges"};
@@ -17,6 +18,10 @@ public class Model {
 		}
 		
 	}
+	// TODO: Items itemSearch(string)
+	//##################################################################################################################################################
+	//
+	//##################################################################################################################################################
 	public Users login(String user_name, String password) {
 		Users user = new Users();
 		try {
@@ -28,7 +33,7 @@ public class Model {
 			while (myRs.next()) {
 				user.setUserID(myRs.getString("userID"));
 				user.setLast_name(myRs.getString("last_name"));
-				user.setFirst_name(myRs.getString("fisrt_name"));
+				user.setFirst_name(myRs.getString("first_name"));
 				user.setUserName(myRs.getString("user_name"));
 				user.setPassword(myRs.getString("password"));
 				user.setMail(myRs.getString("email"));
@@ -43,69 +48,67 @@ public class Model {
 		}
 		return user;
 	}
-	public static String addQuery(String TableName, String columns[], String data[]) {
-		String query = "INSERT INTO `" + TableName + "` (";
-		int i, j;
-		for (i = 0; i < columns.length - 1; i++) {
-			query += columns[i] + ", ";
-		}
-		query += columns[i] + ") VALUES (";
-		for (j = 0; j < data.length - 1; j++) {
-			query += "'"+ data[j] + "', ";
-		}
-		query += "'"+ data[i] + "'";
-		query += ")";
-		return query;
-	}
-	public static String editQuery(String TableName,String TableKey, String columns[], String data[], String index) {
-		String query = "UPDATE `" + TableName + "` SET ";
-		int i;
-		for (i = 0; i < data.length - 1; i++) {
-			query += columns[i] + " = '" + data[i] + "', ";
-		}
-		query += columns[i] + " = '" + data[i] + "'" + " WHERE " + TableName +"." + TableKey + " = " + index;
-		return query;
-	}
-	
-	public Boolean signUp(String data[]) {
+
+	//##################################################################################################################################################
+	public String signUp(String data[]) {
+		String userID = null;
 		try {
 			Statement myStmt = myCon.createStatement();
 			String query = addQuery("users", users_columns, data);
-			System.out.println(query);
+			String getID = "SELECT userID FROM items ORDER BY userID DESC LIMIT 1";
 			myStmt.executeUpdate(query);
-			return true;
+			ResultSet myRs = myStmt.executeQuery(getID);
+			while (myRs.next()) {
+				userID =  myRs.getString("userID");
+			}
+			return userID;
 		}
 		catch (Exception  exc){
 			exc.printStackTrace();
-			return false;
+			return "0";
 		}
 	}
-	public boolean addItem(String data[]) {
+	//##################################################################################################################################################
+	public String addItem(String data[]) {
+		String itemID = null;
 		try {
 			// Create a statement
 			Statement myStmt = myCon.createStatement();
 			String query = addQuery("items", items_columns, data);
+			String getID = "SELECT itemID FROM items ORDER BY itemID DESC LIMIT 1";
 			myStmt.executeQuery(query);
-			return true;
+			ResultSet myRs = myStmt.executeQuery(getID);
+			while (myRs.next()) {
+				itemID =  myRs.getString("itemID");
+			}
+			return itemID;
 		}
 		catch (Exception  exc){
 			exc.printStackTrace();
-			return false;
+			return "0";
 		}
 	}
-	public boolean addRequest(String data[]) {
+	//##################################################################################################################################################
+	public String addRequest(String data[]) {
+		String requestID = null;
 		try {
 			// Create a statement
 			Statement myStmt = myCon.createStatement();
 			String query = addQuery("requests", requests_columns, data);
+			String getID = "SELECT requestID FROM items ORDER BY requestID DESC LIMIT 1";
 			myStmt.executeQuery(query);
-			return true;
+			ResultSet myRs = myStmt.executeQuery(getID);
+			while (myRs.next()) {
+				requestID =  myRs.getString("requestID");
+			}
+			return requestID;
 		}
 		catch (Exception  exc){
 			exc.printStackTrace();
-			return false;
+			return "0";
 		}
 	}
+	//##################################################################################################################################################
 	public boolean editUser(String columns[], String data[], String userID) {
 		try {
 			// Create a statement
@@ -119,6 +122,7 @@ public class Model {
 			return false;
 		}
 	}
+	//##################################################################################################################################################
 	public boolean editItem(String columns[], String data[], String itemID) {
 		try {
 			// Create a statement
@@ -132,6 +136,7 @@ public class Model {
 			return false;
 		}
 	}
+	//##################################################################################################################################################
 	public boolean editRequest(String columns[], String data[], String requestID) {
 		try {
 			// Create a statement
@@ -145,4 +150,73 @@ public class Model {
 			return false;
 		}
 	}
+	//##################################################################################################################################################
+	public boolean responseRequest(String requestID, String response) {
+		try {
+			// Create a statement
+			String[] columns = {"response"};
+			String[] data = {response};
+			Statement myStmt = myCon.createStatement();
+			String query = editQuery("requests", "requestID", columns, data, requestID);
+			myStmt.executeQuery(query);
+			return true;
+		}
+		catch (Exception  exc){
+			exc.printStackTrace();
+			return false;
+		}
+	}
+	//##################################################################################################################################################
+	// input: 
+	// output:
+	//##################################################################################################################################################
+	public static String addQuery(String TableName, String columns[], String data[]) {
+		String queryA = "INSERT INTO `" + TableName + "` (";
+		int i, j;
+		for (i = 0; i < columns.length - 1; i++) {
+			queryA += columns[i] + ", ";
+		}
+		queryA += columns[i] + ") VALUES (";
+		for (j = 0; j < data.length - 1; j++) {
+			queryA += "'"+ data[j] + "', ";
+		}
+		queryA += "'"+ data[i] + "'";
+		queryA += ")";
+		return queryA;
+	}
+	//##################################################################################################################################################
+	public static String editQuery(String TableName,String TableKey, String columns[], String data[], String index) {
+		String query = "UPDATE `" + TableName + "` SET ";
+		int i;
+		for (i = 0; i < data.length - 1; i++) {
+			query += columns[i] + " = '" + data[i] + "', ";
+		}
+		query += columns[i] + " = '" + data[i] + "'" + " WHERE " + TableName +"." + TableKey + " = " + index;
+		return query;
+	}
+	// TODO: String removeQuery(String index)
+	//##################################################################################################################################################
+	//
+	// TODO: String getListQuery(String Table)
+	//##################################################################################################################################################
+	//
+	// TODO: boolean removeUser(String userID)
+	//##################################################################################################################################################
+	//
+	// TODO: boolean removeItem(String itemID)
+	//##################################################################################################################################################
+	//
+	// TODO: boolean removeRequest(String requestID)
+	//##################################################################################################################################################
+	//
+	// TODO: Users[] getUserList(String userID)
+	//##################################################################################################################################################
+	//
+	// TODO: Items[] getItemsList(String itemID)
+	//##################################################################################################################################################
+	//
+	// TODO: Requests[] getRequestsList(String requestID)
+	//##################################################################################################################################################
+	//
+	//##################################################################################################################################################	
 }
